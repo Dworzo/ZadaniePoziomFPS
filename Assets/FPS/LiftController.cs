916a225c57161294e6ae0d12feb2288ec9daa496
Dworzo <dworzo@gmail.com>
2021-09-18 23:52:26 +0200

    Winda zrobiona

--- Assets/FPS/LiftController.cs
+++ Assets/FPS/LiftController.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class LiftController : MonoBehaviour
 {
-
+    public GameObject instructions;
     private void OnTriggerStay(Collider other)
     {
         if(other.tag == "Lift")
         {
+            instructions.SetActive(true);
             Animator anim = other.GetComponentInChildren<Animator>();
             if (Input.GetKeyDown(KeyCode.F))
                 anim.SetTrigger("DownUp");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Lift")
+        {
+            instructions.SetActive(false);
         }
     }
 }
